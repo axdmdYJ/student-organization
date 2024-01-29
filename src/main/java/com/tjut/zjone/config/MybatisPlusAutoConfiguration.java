b0d03216c24474321ac3738b1437f5e0ab2b9986
axdmdyj <2991142800@qq.com>
2024-01-29 21:35:44 +0800

    feature: 引入MP分页插件

---
+++ src/main/java/com/tjut/zjone/config/MybatisPlusAutoConfiguration.java
@@ -0,0 +1,32 @@
+
+package com.tjut.zjone.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * MybatisPlus 配置文件
+ *
+ */
+public class MybatisPlusAutoConfiguration {
+
+    /**
+     * 分页插件
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+
+    /**
+     * 元数据填充
+     */
+    @Bean
+    public MyMetaObjectHandler myMetaObjectHandler() {
+        return new MyMetaObjectHandler();
+    }
+}
