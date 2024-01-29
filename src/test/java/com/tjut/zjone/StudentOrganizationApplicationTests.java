6e1341604a51a606a6ea43e6a5ca875c9ca10d59
axdmdyj <2991142800@qq.com>
2024-01-29 10:54:20 +0800

    feature: 项目结构初始化

---
+++ src/test/java/com/tjut/zjone/StudentOrganizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tjut.zjone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentOrganizationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
