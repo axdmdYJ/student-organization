d4767cdd1ce6142de2b255dceee66dc29d7f74cd
axdmdyj <2991142800@qq.com>
2024-03-04 22:30:12 +0800

    optimize: 简化缓存实现

--- src/main/java/com/tjut/zjone/common/biz/user/UserSessionHelper.java
+++ src/main/java/com/tjut/zjone/common/biz/user/UserSessionHelper.java
@@ -7,17 +7,19 @@ import com.auth0.jwt.interfaces.DecodedJWT;
 import com.auth0.jwt.interfaces.JWTVerifier;
 import com.tjut.zjone.util.JsonUtil;
 import com.tjut.zjone.util.MapUtils;
-import com.tjut.zjone.cache.RedisClient;
 import com.tjut.zjone.util.SelfTraceIdGenerator;
+import jakarta.annotation.Resource;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.util.Base64Utils;
 
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Objects;
+import java.util.concurrent.TimeUnit;
 
 /**
  * 使用jwt来存储用户token，则不需要后端来存储session了
@@ -46,6 +48,8 @@ public class UserSessionHelper  {
 
     private final JwtProperties jwtProperties;
 
+    @Resource
+    StringRedisTemplate stringRedisTemplate;
     private Algorithm algorithm;
     private JWTVerifier verifier;
 
@@ -63,12 +67,13 @@ public class UserSessionHelper  {
                 .sign(algorithm);
         // 2.使用jwt生成的token时，后端可以不存储这个session信息, 完全依赖jwt的信息
         // 但是需要考虑到用户登出，需要主动失效这个token，而jwt本身无状态，所以再这里的redis做一个简单的token -> userId的缓存，用于双重判定
-        RedisClient.setStrWithExpire(token, username, jwtProperties.getExpire() / 1000);
+        stringRedisTemplate.opsForValue().set(token, username);
+        stringRedisTemplate.expire(token, jwtProperties.getExpire() / 1000, TimeUnit.MINUTES);
         return token;
     }
 
     public void removeSession(String session) {
-        RedisClient.del(session);
+        stringRedisTemplate.delete(session);
     }
 
     /**
@@ -86,7 +91,7 @@ public class UserSessionHelper  {
             String username = String.valueOf(JsonUtil.toObj(pay, HashMap.class).get("u"));
 
             // 从redis中获取username，解决用户登出，后台失效jwt token的问题
-            String user = RedisClient.getStr(session);
+            String user = stringRedisTemplate.opsForValue().get(session);
             if (user == null || !Objects.equals(username, user)) {
                 return null;
             }
